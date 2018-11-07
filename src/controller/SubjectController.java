bfe1a169f2fc4e20bd16dab165f7a211ce0d2d5f
455916695 <455916695@qq.com>
2018-11-07 20:39:56 +0800

    end 修改版本

--- src/controller/SubjectController.java
+++ src/controller/SubjectController.java
@@ -1,14 +1,18 @@
 package controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pojo.Subject;
 import service.SubjectService;
+import utils.JsonUtils;
 import utils.Result;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/subject")
@@ -28,10 +32,32 @@ public class SubjectController {
         }catch (Exception e){
             result = Result.build(Result.ERROR_STATUS_CODE,"课表查询失败");
         }
+
         return  result;
     }
 
+    /**
+     *   根据学号和学期查询课表信息，如果没有学期参数，查询最新学期的课表
+     *
+     * */
+    @RequestMapping(value = "/findSubjectJsonP",produces= MediaType.APPLICATION_JSON_VALUE+";charset=utf-8")
+    public  String findSubject(@RequestParam(defaultValue = "") String XH, @RequestParam(defaultValue = "") String XQDM,String jsonpCallback){
+        Result result = null;
+        try {
+            List<List<List<Subject>>> subjectByXHAndXQDM = subjectServiceImpl.findSubjectByXHAndXQDM(XH, XQDM);
+            result = Result.ok(subjectByXHAndXQDM);
+        }catch (Exception e){
+            result = Result.build(Result.ERROR_STATUS_CODE,"课表查询失败");
+        }
+        if(!StringUtils.isEmpty(jsonpCallback)) {
+            return jsonpCallback+"("+ JsonUtils.objectToJson(result)+");";
+        }
+        return JsonUtils.objectToJson(result);
+    }
 
+/**
+ *  查询学期代码
+ * */
     @RequestMapping("/findXQDM")
     public Result findXQDM(){
         Result result = null;
@@ -42,6 +68,44 @@ public class SubjectController {
             result = Result.build(Result.ERROR_STATUS_CODE,"学期查询失败");
         }
         return result;
+    }
+
+    /**
+     *  查询学期代码 JsonP 版
+     * */
+    @RequestMapping("/findXQDMJonP")
+    public String findXQDM(String jsonpCallback){
+        Result result = null;
+        try {
+            List<String> xqdm = subjectServiceImpl.findXQDM();
+            result = Result.ok(xqdm);
+        }catch (Exception e){
+            result = Result.build(Result.ERROR_STATUS_CODE,"学期查询失败");
+        }
+
+        if(!StringUtils.isEmpty(jsonpCallback)) {
+            return jsonpCallback+"("+ JsonUtils.objectToJson(result)+");";
+        }
+        return JsonUtils.objectToJson(result);
+    }
+
+    /**
+     *  查询学年
+     * */
+    @RequestMapping("/findXNJonP")  //TODO 未测
+    public String findXN(String jsonpCallback){
+        Result result = null;
+        try {
+            Set<String> xn =  subjectServiceImpl.findXN();
+            result = Result.ok(xn);
+        }catch (Exception e){
+            result = Result.build(Result.ERROR_STATUS_CODE,"查询学年失败");
+        }
+        if (!StringUtils.isEmpty(jsonpCallback)){
+            return jsonpCallback+"(" + JsonUtils.objectToJson(result) +")";
+        }
+
+        return JsonUtils.objectToJson(result);
     }
 
 
--- src/service/impl/SubjectServiceImpl.java
+++ src/service/impl/SubjectServiceImpl.java
@@ -1,13 +1,17 @@
 package service.impl;
 
+import com.github.pagehelper.util.StringUtil;
 import dao.SubjectDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 import pojo.Subject;
 import service.SubjectService;
 import utils.SubjectUtil;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class SubjectServiceImpl implements SubjectService {
@@ -16,26 +20,58 @@ public class SubjectServiceImpl implements SubjectService {
     private SubjectDao subjectDao;
 
     /**
-     *  根据学号  和 学期 信息 查询课表
-     *  @param  XH  学号
-     *  @param  XQDM  学期代码
-     * */
+     * 根据学号  和 学期 信息 查询课表
+     *
+     * @param XH   学号
+     * @param XQDM 学期代码
+     */
     public List<List<List<Subject>>> findSubjectByXHAndXQDM(String XH, String XQDM) {
+
+        if (StringUtils.isEmpty(XQDM)) {
+            XQDM = findXQDMNew(); //查询出最新的学期
+        }
 
         List<Subject> subjectList = subjectDao.findSubjetByXHAndXQDM(XH, XQDM);
 
-//        对结果进行遍历并进行处理
+        //        对结果进行遍历并进行处理
         return SubjectUtil.processData(subjectList);
 
     }
 
     /**
-     *  查询学期代码
-     * */
+     * 查询学期代码，
+     */
     public List<String> findXQDM() {
         List<String> xqdm = subjectDao.findXQDM();
-        return  xqdm;
+        return xqdm;
     }
 
+    /**
+     * 查询学年
+     */
+    public Set<String> findXN() {
+        List<String> xqdm = findXQDM(); //学期代码
+
+        Set<String> xn = new HashSet<>();  //学年
+
+        for (String xq : xqdm) {
+            xn.add(xq.substring(0, xq.length() - 1)); //截取学年
+        }
+        return xn;
+    }
+
+    /**
+     * 查询最新的学期信息
+     */
+    public String findXQDMNew() {
+        List<String> xqdm = findXQDM();
+        String result = xqdm.get(0);
+        for (String xq : xqdm) {
+            if (result.compareTo(xq) < 0) {
+                result = xq;
+            }
+        }
+        return result;
+    }
 
 }
