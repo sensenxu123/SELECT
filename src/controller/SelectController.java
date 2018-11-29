0bb43f5f1eb883f32d961aa4a143ebfeccb56924
sensenxu123 <975694749@qq.com>
2018-11-29 20:33:41 +0800

    ojbk

--- src/controller/SelectController.java
+++ src/controller/SelectController.java
@@ -36,7 +36,7 @@ public class SelectController {
     @RequestMapping(value="/checklogin",produces = "application/json;charset=utf-8")
     //这个方法是校验登录
     @ResponseBody
-    public String login(String xgh,String password,String callback){
+    public String login(String xgh,String password,String callback,HttpSession session){
         Result result=selectService.checkLogin(xgh,password);
         return callback+"("+JsonUtils.objectToJson(ResultUtil.success("查询成功",result))+");";
     }
@@ -45,93 +45,27 @@ public class SelectController {
     @RequestMapping(value="/selectRecord",produces = "application/json;charset=utf-8")
     @ResponseBody
     public String selectRecord(String xgh,HttpSession session,String callback) {
-        //先从session中拿出time 如果没有则为默认查询当月
-        String mintime = (String) session.getAttribute("mintime");
-        String maxtime = (String) session.getAttribute("maxtime");
         int pageSize=20;
-        if(mintime==null&&maxtime==null){
-//            //固定显示条数
-//            if(currentPage==0)currentPage=1;
-//            PageHelper.startPage(currentPage, pageSize);
+        //先从session中拿出time 如果没有则为默认查询当月
             List<Record> docs = selectService.selectByPageAndSelections(xgh);
-//            PageInfo<Record> pageInfo = new PageInfo<>(docs);
-//            //从查询中获取总页数
-//            int pages = pageInfo.getPages();
-//            int total =(int) pageInfo.getTotal();
-//            //将总页数，第几页，每页显示条数写入PageBean实体
-//            PageBean<Object> pageBean = new PageBean<>(currentPage,pageSize,total,pages);
-//            List<Record> list = pageInfo.getList();
-//            //索引list
-//            int count=(currentPage-1)*20;
-//            List<Integer> integerList=new ArrayList<>();
-//            for(int i=1;i<=list.size();i++){
-//                integerList.add(count+i);
-//            }
-//            List list1=new ArrayList();
-//            list1.add(list);
-//            list1.add(integerList);
-//            list1.add(pageBean);
+
             return callback+"("+JsonUtils.objectToJson(ResultUtil.success("查询成功",docs))+");";
-        }
-        else{
-//            PageHelper.startPage(currentPage, pageSize);
-            List<Record> recordList= selectService.selectRecoredBytime(mintime,maxtime,xgh);
-//            PageInfo<Record> pageInfo = new PageInfo<>(recordList);
-//            List<Record> list = pageInfo.getList();
-            //从查询中获取总页数ml;
-//            int pages = pageInfo.getPages();
-//            int total =(int) pageInfo.getTotal();
-//            PageBean<Object> pageBean = new PageBean<>(currentPage,pageSize,total,pages);
-//            int count=(currentPage-1)*20;
-//            List<Integer> integerList=new ArrayList<>();
-//            for(int i=1;i<=list.size();i++){
-//                integerList.add(count+i);
-//            }
-//            List list1=new ArrayList();
-//            list1.add(list);
-//            list1.add(integerList);
-//            list1.add(pageBean);
-            return callback+"("+JsonUtils.objectToJson(ResultUtil.success("查询成功",recordList))+");";
-        }
 
-
-
-
-
-
-
-//        ModelAndView mv = new ModelAndView();
-//        mv.addObject("list",list);
-//        mv.addObject("integerList",integerList);
-//        mv.addObject("pageBean",pageBean);
-//        mv.setViewName("test");
     }
+
+
+
     @RequestMapping(value="/selectRecoredBytime",produces = "application/json;charset=utf-8")
     //这个方法是查询账户的明细
     public String selectRecoredBytime(String xgh,String mintime,String maxtime,HttpSession session,String callback){
         //将两个时间放入session域中
-        session.setAttribute("mintime",mintime);
-        session.setAttribute("maxtime",maxtime);
-//        int currentPage=1;
-//        int pageSize=20;
-//        PageHelper.startPage(currentPage, pageSize);
+
         List<Record> recordList= selectService.selectRecoredBytime(mintime,maxtime,xgh);
-//        PageInfo<Record> pageInfo = new PageInfo<>(recordList);
-//        List<Record> list = pageInfo.getList();
-//        //从查询中获取总页数ml;mklmkn
-//        int pages = pageInfo.getPages();
-//        int total =(int) pageInfo.getTotal();
-//        PageBean<Object> pageBean = new PageBean<>(currentPage,pageSize,total,pages);
-//        int count=(currentPage-1)*20;
-//        List<Integer> integerList=new ArrayList<>();
-//        for(int i=1;i<=list.size();i++){
-//            integerList.add(count+i);
-//        }
-//        List list1=new ArrayList();
-//        list1.add(list);
-//        list1.add(integerList);
-//        list1.add(pageBean);
+
         return callback+"("+JsonUtils.objectToJson(ResultUtil.success("查询成功",recordList))+");";
 
     }
+
+
+
 }
--- src/test/test.java
+++ src/test/test.java
@@ -1,11 +1,24 @@
 package test;
 
+import org.apache.http.HttpRequest;
+import org.apache.struts.chain.contexts.ActionContext;
+import org.apache.struts.chain.contexts.ServletActionContext;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class test {
+
+
+    @Autowired
+
+    private HttpServletRequest request;
+
+
     @Test
     public void test(){
         Date date = new Date();
@@ -23,9 +36,15 @@ public class test {
     }
     @Test
     public  void test2(){
-        String YE="99";
-        float pwd = Integer.parseInt(YE)/100.00f;
-        System.out.println(pwd+"");
+        try{
+            System.out.println(request.getAttribute("ss"));
+        }catch(Exception e){
+            System.out.println("asd");
+        }
+
+
+
+
     }
 
 }
